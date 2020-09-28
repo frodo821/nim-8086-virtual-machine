0a1a301852882d904eccc664f1cb4b75dbc1002f
frodo821 <sakaic2003@gmail.com>
2020-09-29 03:37:10 +0900

    スタック操作の命令をマクロに書き換え

--- src/instructions/stack.nim
+++ src/instructions/stack.nim
@@ -1,4 +1,5 @@
 import ../hardware
+import ../internalmacros
 
 proc pushR32*(cpu: Cpu) =
   let reg = cpu.getU8(0) - 0x50
@@ -10,10 +11,10 @@ proc popR32*(cpu: Cpu) =
   cpu.setR32(reg, cpu.pop32())
   cpu.eip += 1
 
-proc pushImm8*(cpu: Cpu) =
+inst pushImm8, 0x6a:
   cpu.push8(cpu.getU8(1))
   cpu.eip += 2
 
-proc pushImm32*(cpu: Cpu) =
+inst pushImm32, 0x68:
   cpu.push32(cpu.getU32(1))
   cpu.eip += 5

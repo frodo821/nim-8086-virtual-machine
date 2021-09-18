a3bc36816e222bb2b7f8cc5f9fc6c59d64562546
chihiro sakai <sakaic2003@gmail.com>
2021-09-18 16:43:51 +0900

    other.nimを一番最後に読み込みように修正

--- src/instructions.nim
+++ src/instructions.nim
@@ -2,7 +2,7 @@ import ./instructions/arithmetic
 import ./instructions/instmacros
 import ./instructions/jmp
 import ./instructions/mov
-import ./instructions/others
 import ./instructions/stack
+import ./instructions/others
 
 export arithmetic, jmp, mov, others, stack, loadAllInsts

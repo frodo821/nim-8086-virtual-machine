23fd0dd6f00d0efb1fb15f0bbb879fda2017ecff
chihiro sakai <sakaic2003@gmail.com>
2021-09-18 16:44:20 +0900

    プレフィクス付き命令を定義できるようにマクロを追加

--- src/instructions/instmacros.nim
+++ src/instructions/instmacros.nim
@@ -1,6 +1,7 @@
-import macros
+import macros, tables, strutils
 
 var insts {.compileTime.}: seq[NimNode] = newSeq[NimNode]()
+var prefixes {.compileTime.}: Table[uint8, seq[NimNode]] = initTable[uint8, seq[NimNode]]()
 
 macro inst*(name: untyped, opcode: uint8, body: untyped): untyped =
   let cpu = newIdentNode("cpu")
@@ -35,13 +36,63 @@ macro jumpIf*(name: untyped, opcode: uint8, cond: untyped): untyped =
       else:
         cpu.eip += 2
 
-macro jumpIfNot*(name: untyped, opcode: uint8, cond: untyped): untyped =
+macro jumpUnless*(name: untyped, opcode: uint8, cond: untyped): untyped =
   result = quote do:
     inst `name`, `opcode`:
       if not (`cond`):
         cpu.jmps()
       else:
         cpu.eip += 2
+
+macro instWithPrefix*(name: untyped, prefix: uint8, opcode: uint8, body: untyped): untyped =
+  let pf = cast[uint8](prefix.intVal)
+
+  if pf notin prefixes:
+    prefixes[pf] = @[]
+
+  let cpu = newIdentNode("cpu")
+
+  let caseof = quote do:
+      case 0:
+      of `opcode`:
+        `body`
+
+  prefixes[pf].add(caseof[1])
+
+  result = quote do:
+    proc `name`*(`cpu`: Cpu) =
+      `body`
+
+macro prefixInst*(opcode: uint8): untyped =
+  let code: uint8 = cast[uint8](opcode.intVal)
+
+  if code notin prefixes:
+    return newEmptyNode()
+
+  let insts = newNimNode(nnkCaseStmt)
+  insts.add(nnkCall.newTree(nnkDotExpr.newTree(newIdentNode("cpu"), newIdentNode("getU8")), newLit(0)))
+  insts.add(prefixes[code])
+  
+  insts.add(
+    nnkElse.newTree(
+      nnkStmtList.newTree(
+        nnkRaiseStmt.newTree(
+          nnkCall.newTree(
+            newIdentNode("newException"),
+            newIdentNode("ValueError"),
+            newLit("unknown operator")
+          )
+        )
+      )
+    )
+  )
+
+  let sym = newIdentNode("i"&opcode.intVal.toHex&"H")
+
+  result = quote do:
+    inst `sym`, `opcode`:
+      cpu.eip += 1
+      `insts`
 
 macro loadAllInsts*(ident: untyped): untyped =
   let cpu = newIdentNode("cpu")
